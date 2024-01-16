a63eb4ea39829c5cd87bec5e1f6d65e6c35da9c8
Alejandro Gomez <alexserverone@gmail.com>
2024-01-15 21:37:47 -0600

    Improving Searchbar so it updates url params when text field changes. Also catching those params in Page and sending them to API (query and pagination)

--- nextjs/src/app/components/Searchbar.tsx
+++ nextjs/src/app/components/Searchbar.tsx
@@ -3,13 +3,39 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export const Searchbar = () => {
+  const searchParams = useSearchParams();
+  const [query, setQuery] = useState("");
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = () => {
+    const params = new URLSearchParams(searchParams);
+    if (query) {
+      params.set("query", query);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
   return (
-    <div className="flex gap-2">
-      <Label htmlFor="ticker">Stock symbol or name</Label>
-      <Input id="ticker" name="ticker" />
-      <Button>Search</Button>
+    <div className="w-full flex gap-2">
+      <Label className="text-base" htmlFor="query">
+        Stock symbol or name
+      </Label>
+      {/* We need to wrap Input into a div with flex-grow to avoid taking full space and crush the label */}
+      <div className="flex-grow">
+        <Input
+          id="query"
+          name="query"
+          onChange={(e) => setQuery(e.target.value)}
+          defaultValue={searchParams.get("query")?.toString()}
+        />
+      </div>
+      <Button onClick={handleSearch}>Search</Button>
     </div>
   );
 };
--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -3,26 +3,29 @@ import { DataTable } from "./components/DataTable";
 import { getTickers } from "@/api/fake";
 import { tickerColumns } from "./types/ticker-columns";
 
-async function getData() {
-  const page = 1;
-  const query = "";
-  const limit = 20;
-  const { pagination, data } = await getTickers({ limit, page, query });
-  return { pagination, data };
+async function getData({ page = 1, query = "", limit = 20 }) {
+  return getTickers({ limit, page, query });
 }
 
-export default async function Home() {
-  const { data } = await getData();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const page = Number(searchParams?.page) || 1;
+  const { data } = await getData({ page, query });
   return (
     <main className="flex flex-col items-center justify-start p-10 w-full gap-4">
-      <div className="w-full">
+      <div className="w-full text-center">
         <h1 className="text-3xl text-sky-700">The Broker</h1>
         <h2 className="text-xl">Stock Market Info</h2>
       </div>
       <Searchbar />
-      <div>
-        <DataTable columns={tickerColumns} data={data} />
-      </div>
+      <DataTable columns={tickerColumns} data={data} />
     </main>
   );
 }
