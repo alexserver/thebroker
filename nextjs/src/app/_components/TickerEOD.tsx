4a8904f2e167938951599895ea40272dcf4d2f9c
Alejandro Gomez <alexserverone@gmail.com>
2024-01-18 14:28:13 -0600

    Updating URL params when date changes in DatePicker in TickerEOD component.

--- nextjs/src/app/_components/TickerEOD.tsx
+++ nextjs/src/app/_components/TickerEOD.tsx
@@ -4,28 +4,54 @@ import { Label } from "@/components/ui/label";
 import { DatePicker } from "./DatePicker";
 import type { Ticker } from "../_types/ticker";
 import type { TickerEOD as TickerEODType } from "../_types/ticker-eod";
+import { useEffect, useState } from "react";
+import { format, parse } from "date-fns";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import "./ticker-eod.css";
-import { useState } from "react";
 
 interface TickerEODInfoProps {
   ticker: Ticker;
   eod: TickerEODType;
 }
 
+const currencyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+const integerFormatter = new Intl.NumberFormat("en-US");
+
 export const TickerEOD = ({ ticker, eod }: TickerEODInfoProps) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
   const [date, setDate] = useState<Date>();
-  const currencyFormatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-  const integerFormatter = new Intl.NumberFormat("en-US");
+
+  const onDateChange = (value: Date | undefined) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("eod", format(value, "yyyy-MM-dd"));
+    } else {
+      params.delete("eod");
+    }
+    setDate(value);
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  useEffect(() => {
+    // if URL contains eod param, we set the date so it's passed down to DatePicker
+    const dt = searchParams.get("eod");
+    if (dt) {
+      setDate(parse(dt, "yyyy-MM-dd", new Date()));
+    }
+  }, [searchParams]);
+
   return (
     <div className="ticker-eod">
-      <h1>End of the Day details</h1>
+      <h1 className="title">End of the Day details</h1>
 
       <div className="ticker-eod-controls">
         <Label>Date: </Label>
-        <DatePicker value={date} onChange={setDate} />
+        <DatePicker value={date} onChange={onDateChange} />
       </div>
       {!eod ? (
         <h2 className="no-data">
