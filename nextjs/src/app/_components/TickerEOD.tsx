55de0b3b743d00b6b69264b8c653721dbbbb6d39
Alejandro Gomez <alexserverone@gmail.com>
2024-01-18 11:12:24 -0600

    Adding a DatePicker from shadcn-ui. Also showing it in TickerEOD component.

--- nextjs/src/app/_components/TickerEOD.tsx
+++ nextjs/src/app/_components/TickerEOD.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { Label } from "@/components/ui/label";
+import { DatePicker } from "./DatePicker";
 import type { Ticker } from "../_types/ticker";
 import type { TickerEOD as TickerEODType } from "../_types/ticker-eod";
 import "./ticker-eod.css";
+import { useState } from "react";
 
 interface TickerEODInfoProps {
   ticker: Ticker;
@@ -8,6 +13,7 @@ interface TickerEODInfoProps {
 }
 
 export const TickerEOD = ({ ticker, eod }: TickerEODInfoProps) => {
+  const [date, setDate] = useState<Date>();
   const currencyFormatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -15,6 +21,12 @@ export const TickerEOD = ({ ticker, eod }: TickerEODInfoProps) => {
   const integerFormatter = new Intl.NumberFormat("en-US");
   return (
     <div className="ticker-eod">
+      <h1>End of the Day details</h1>
+
+      <div className="ticker-eod-controls">
+        <Label>Date: </Label>
+        <DatePicker value={date} onChange={setDate} />
+      </div>
       {!eod ? (
         <h2 className="no-data">
           There&apos;s no data for this date on Stock Market
