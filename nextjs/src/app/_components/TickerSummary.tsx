9d48bdfbf73033ddbed0a47aba8ff6b1579b564d
Alejandro Gomez <alexserverone@gmail.com>
2024-01-18 20:02:14 -0600

    moving styling to ticker-summary css

--- nextjs/src/app/_components/TickerSummary.tsx
+++ nextjs/src/app/_components/TickerSummary.tsx
@@ -1,4 +1,5 @@
 import type { Ticker } from "../_types/ticker";
+import "./ticker-summary.css";
 
 interface TickerSummaryProps {
   ticker: Ticker;
@@ -6,25 +7,25 @@ interface TickerSummaryProps {
 
 export const TickerSummary = ({ ticker }: TickerSummaryProps) => {
   return (
-    <div className="flex gap-2 lg:gap-4 flex-wrap justify-between lg:justify-start text-base text-sky-800">
-      <p className="flex flex-col lg:flex-row gap-2">
-        <span className="font-bold text-blue-700">Symbol: </span>
+    <div className="ticker-summary">
+      <p>
+        <span className="label">Symbol: </span>
         {ticker.symbol}
       </p>
-      <p className="flex flex-col lg:flex-row gap-2">
-        <span className="font-bold text-blue-700">Name: </span>
+      <p>
+        <span className="label">Name: </span>
         {ticker.name}
       </p>
-      <p className="flex flex-col lg:flex-row gap-2">
-        <span className="font-bold text-blue-700">Stock Exchange: </span>
+      <p>
+        <span className="label">Stock Exchange: </span>
         {ticker.stock_exchange.name}
       </p>
-      <p className="flex flex-col lg:flex-row gap-2">
-        <span className="font-bold text-blue-700">Symbol: </span>
+      <p>
+        <span className="label">Symbol: </span>
         {ticker.stock_exchange.acronym}
       </p>
-      <p className="flex flex-col lg:flex-row gap-2">
-        <span className="font-bold text-blue-700">Country: </span>
+      <p>
+        <span className="label">Country: </span>
         {ticker.stock_exchange.country}
       </p>
     </div>
