455330654f845a47b29bca238a7f79aa4f6b1fe0
Alejandro Gomez <alexserverone@gmail.com>
2024-03-12 19:43:17 -0600

    Improving the Chart component. Now showing all pivot values except volume (for now) The dropdown only decides whether the chart is a line or an area. Adding different colors for the Chart lines/areas

--- src/app/_components/Chart.tsx
+++ src/app/_components/Chart.tsx
@@ -9,11 +9,14 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  AreaChart,
+  Area,
 } from "recharts";
 import { compareAsc, format } from "date-fns";
 import { useMemo } from "react";
 
-interface ChartOptions {
+type data_key = "open" | "close" | "high" | "low" | "volume";
+export interface ChartOptions {
   data: Array<{
     date: string;
     open?: number;
@@ -22,10 +25,23 @@ interface ChartOptions {
     low?: number;
     volume?: number;
   }>;
-  data_key: string;
+  data_keys: Array<data_key>;
+  chart_type?: "line" | "area";
 }
 
-export default function Chart({ data, data_key }: ChartOptions) {
+const colors = {
+  open: "#265073",
+  close: "#2D9596",
+  high: "#FF407D",
+  low: "#40679E",
+  volume: "#5F5D9C",
+};
+
+export default function Chart({
+  data,
+  data_keys,
+  chart_type = "line",
+}: ChartOptions) {
   // memoize a transformation of data array
   // 1) sort asc by date
   // 2) format date to "MMM d" -> (Nov 1)
@@ -42,19 +58,67 @@ export default function Chart({ data, data_key }: ChartOptions) {
     [data]
   );
 
-  return (
-    <ResponsiveContainer width="100%" height={300}>
-      <LineChart
-        data={transformedData}
-        margin={{ top: 5, right: 5, bottom: 5, left: 5 }}
-      >
-        <CartesianGrid strokeDasharray="1" />
-        <XAxis dataKey="date" />
-        <YAxis domain={["dataMin", "dataMax"]} />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey={data_key} stroke="#020617" />
-      </LineChart>
-    </ResponsiveContainer>
-  );
+  if (chart_type === "line")
+    return (
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          data={transformedData}
+          margin={{ top: 5, right: 5, bottom: 5, left: 5 }}
+        >
+          <CartesianGrid strokeDasharray="1" />
+          <XAxis dataKey="date" />
+          <YAxis domain={["dataMin", "dataMax"]} />
+          <Tooltip />
+          <Legend />
+          {data_keys.map((key) => (
+            <Line
+              key={key}
+              type="monotone"
+              dataKey={key}
+              stroke={colors[key]}
+            />
+          ))}
+        </LineChart>
+      </ResponsiveContainer>
+    );
+
+  if (chart_type === "area" && data_keys.length > 0)
+    return (
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart
+          data={transformedData}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            {data_keys.map((key) => (
+              <linearGradient
+                key={key}
+                id={`color${key}`}
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              >
+                <stop offset="5%" stopColor={colors[key]} stopOpacity={0.8} />
+                <stop offset="95%" stopColor={colors[key]} stopOpacity={0} />
+              </linearGradient>
+            ))}
+          </defs>
+          <XAxis dataKey="date" />
+          <YAxis domain={["dataMin", "dataMax"]} />
+          <CartesianGrid strokeDasharray="1" />
+          <Tooltip />
+          {data_keys.map((key) => (
+            <Area
+              key={key}
+              type="monotone"
+              dataKey={key}
+              stroke={colors[key]}
+              fillOpacity={1}
+              fill={`url(#color${key})`}
+            />
+          ))}
+        </AreaChart>
+      </ResponsiveContainer>
+    );
 }
--- src/app/_components/TickerChart.tsx
+++ src/app/_components/TickerChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Label } from "@/components/ui/label";
-import Chart from "./Chart";
+import Chart, { type ChartOptions } from "./Chart";
 import type { Ticker } from "../_types/ticker";
 import { format, parse, subDays } from "date-fns";
 import { cn } from "@/lib/utils";
@@ -35,7 +35,7 @@ function getDefaultParams(searchParams: ReadonlyURLSearchParams) {
       searchParams.get("h_date_from") ??
       format(subDays(new Date(), 30), "yyyy-MM-dd"),
     date_to: searchParams.get("h_date_to") ?? format(new Date(), "yyyy-MM-dd"),
-    pivot: searchParams.get("h_pivot") ?? "close",
+    chart_type: searchParams.get("chart_type") ?? "area",
   };
 }
 
@@ -49,13 +49,13 @@ export default function TickerChart({ ticker, data }: TickerChartProps) {
       // update the url params
       if (value && (param === "h_date_from" || param === "h_date_to")) {
         params.set(param, format(value, "yyyy-MM-dd"));
-      } else if (value && param === "h_pivot") {
+      } else if (value && param === "chart_type") {
         params.set(param, value as string);
       }
 
       replace(`${pathname}?${params.toString()}`);
     };
-  const { date_from, date_to, pivot } = getDefaultParams(searchParams);
+  const { date_from, date_to, chart_type } = getDefaultParams(searchParams);
   return (
     <div className={cn("ticker-chart", globals.card)}>
       <h1 className={globals.subtitle}>{ticker.symbol} Historical Data</h1>
@@ -78,20 +78,21 @@ export default function TickerChart({ ticker, data }: TickerChartProps) {
           <div className="form-control">
             <Label htmlFor="price_type">Type:</Label>
             <Select
-              value={pivot}
+              value={chart_type}
               options={[
-                { value: "open", label: "Open" },
-                { value: "close", label: "Close" },
-                { value: "high", label: "High" },
-                { value: "low", label: "Low" },
-                { value: "volume", label: "Volume" },
+                { value: "line", label: "Line" },
+                { value: "area", label: "Area" },
               ]}
-              onValueChange={(value) => onParamChange("h_pivot")(value)}
+              onValueChange={(value) => onParamChange("chart_type")(value)}
             />
           </div>
         </div>
         <div className="bg-white">
-          <Chart data={data} data_key={pivot} />
+          <Chart
+            data={data}
+            chart_type={chart_type as ChartOptions["chart_type"]}
+            data_keys={["open", "close", "high", "low"]}
+          />
         </div>
       </div>
     </div>
